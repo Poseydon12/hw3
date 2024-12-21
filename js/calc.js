754ff2f013c83bff6953bde8fdb36d12dfe38a1b
arthur :) <arturkorol3008@gmail.com>
2024-12-21 09:12:01 +0200

    done homework

---
+++ js/calc.js
@@ -0,0 +1,26 @@
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+let email = "your_email@example.com";
+
+let containsAtSymbol = email.includes("@");
+let totalCharacters = email.length;
+
+console.log(`Чи містить символ @: ${containsAtSymbol}`);
+console.log(`Загальна кількість символів: ${totalCharacters}`);
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+
+let fullName = `${word1} ${word2} ${word3} `;
+
+fullName += "Viktor";
+console.log(fullName);
+
+let userName = "Олександро";
+let payment = 300;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
